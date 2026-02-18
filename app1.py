db59ef96264cb82969c7cb72472ed1868fe06b71
alphaba032-alt <alphaba032@gmail.com>
2026-02-18 01:05:05 +0000

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,34 @@
+import streamlit as st
+st.subheader("Geomaticien")
+
+st.markdown("Tehnicien superieur en Geomatique")
+
+st.header("🧰Formation")
+col1,col2= st.columns(2)
+with col1:
+    st.write("""Brevet en technicien superieur en geomatique/CEDT Le G15""")
+with col2:
+    st.write("""Diplome du baccalureat/College privee catholique Jean 23""")
+
+st.header("🛠️Competence")
+col1,col2 =st.columns(2)
+with col1:
+    st.subheader("Geomatique")
+    st.write("✅Utilusation des outils de travail colaboratifs")
+    st.write("✅Modelisation Batiment 2D sur autoCAD")
+    st.write("✅Collecte de donnees avec Mobile topographe")
+    st.write("✅Numerisation Batiment 3D sur Skeshup")
+    st.write("✅Maitrise des outils SIG Systemes d'information Geomatique ")
+
+
+
+with st.sidebar:
+    st.image("profil.jpeg",width=100)
+    st.subheader("🎓ALPHA AMADOU BA")
+    st.subheader("🏠Adresse")
+    st.header("Dakar")
+    st.subheader("✉️Email")
+    st.header("alphaba03@gmail.com")
+    st.subheader("📞Contact")
+    st.header("783810998")
+    
